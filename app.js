dd87455d0cd92161e24a3eadd4e12a9f40bd3da7
Mathis Peingnez <mathis.peingnez@gmail.com>
2018-12-11 16:57:28 +0100

    Deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const passport     = require('passport');
 require("./config/passport-setup.js")
 
 mongoose
-  .connect('mongodb://localhost/ironphones-server', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -41,7 +41,7 @@ app.use(cors({
 }));
 
 app.use(session({
-  secret: "KDOQwh6bSeTad4JHDeMg",
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   store: new MongoStore({ mongooseConnection: mongoose.connection })
---
+++ public/precache-manifest.0489f7ca1e149cb9fe107fa5d9a269b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd0c05014a7d1ea0070e",
+    "url": "/static/js/main.cd0c0501.chunk.js"
+  },
+  {
+    "revision": "bf3c18490c5d45ebdbc1",
+    "url": "/static/js/1.bf3c1849.chunk.js"
+  },
+  {
+    "revision": "cd0c05014a7d1ea0070e",
+    "url": "/static/css/main.ae19d5ab.chunk.css"
+  },
+  {
+    "revision": "294ddb7483967b196e62187345de4c4f",
+    "url": "/index.html"
+  }
+];
